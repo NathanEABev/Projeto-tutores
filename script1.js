b75f394325d1d277762356bd9489a0214cf84a37
Nathan E A Bevilaqua <nathanarrotheia@gmail.com>
2025-04-30 11:55:24 -0300

    v001.1

--- script1.js
+++ script1.js
@@ -4,6 +4,8 @@ const abPSala = document.getElementById("atPopSala")
 const PSala = document.getElementById("popSala")
 const overlay1 = document.getElementById("overlay1")
 const fechaSala = document.getElementById("fechaPop")
+const enviaSala = document.getElementById("serAlt")
+
 abPSala.addEventListener("click", function() {
     PSala.style.display = "flex"
     overlay1.style.display = "block"
@@ -11,5 +13,12 @@ abPSala.addEventListener("click", function() {
     fechaSala.addEventListener("click", function() {
         PSala.style.display = "none"
     overlay1.style.display = "none"
+    })
+
+    enviaSala.addEventListener("click", function() {
+        alert("Substitui pelo código do db")
+
+        PSala.style.display = "none"
+        overlay1.style.display = "none"
     })
 })
